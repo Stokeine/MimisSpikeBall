80cac4b678ffb61e9ab1657decd4e15a4617f310
MathisLepage <mathislepageb@gmail.com>
2025-05-25 12:07:09 -0400

    League.fxml fait

--- MimisSpikeBall/src/main/java/org/example/controller/LeagueController.java
+++ MimisSpikeBall/src/main/java/org/example/controller/LeagueController.java
@@ -3,10 +3,15 @@ package org.example.controller;
 import javafx.collections.ObservableList;
 import org.example.models.Equipe;
 import org.example.models.Joueur;
+import org.example.models.Match;
 
 import java.util.ArrayList;
+import java.util.List;
 
 public class LeagueController {
+
+    private List<Match> matches;
+    private List<Equipe> equipes;
     public void setEquipes(ObservableList<Equipe> equipes) {
     }
 
